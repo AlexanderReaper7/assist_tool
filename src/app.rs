ad17b1f9251b9da0a10e888d6c590a7a6e9a8e7d
Alexander Öberg <stativetroller7@gmail.com>
2022-01-22 03:48:08 +0100

    renamed and persist by default

--- src/app.rs
+++ src/app.rs
@@ -1,18 +1,18 @@
 use eframe::{egui, epi};
 
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
-#[cfg_attr(feature = "persistence", derive(serde::Deserialize, serde::Serialize))]
-#[cfg_attr(feature = "persistence", serde(default))] // if we add new fields, give them default values when deserializing old state
-pub struct TemplateApp {
+#[derive(serde::Deserialize, serde::Serialize)]
+#[serde(default)] // if we add new fields, give them default values when deserializing old state
+pub struct AssistApp {
     // Example stuff:
     label: String,
 
     // this how you opt-out of serialization of a member
-    #[cfg_attr(feature = "persistence", serde(skip))]
+    #[serde(skip)]
     value: f32,
 }
 
-impl Default for TemplateApp {
+impl Default for AssistApp {
     fn default() -> Self {
         Self {
             // Example stuff:
@@ -22,9 +22,9 @@ impl Default for TemplateApp {
     }
 }
 
-impl epi::App for TemplateApp {
+impl epi::App for AssistApp {
     fn name(&self) -> &str {
-        "eframe template"
+        "AssistApp"
     }
 
     /// Called once before the first frame.
@@ -36,7 +36,6 @@ impl epi::App for TemplateApp {
     ) {
         // Load previous app state (if any).
         // Note that you must enable the `persistence` feature for this to work.
-        #[cfg(feature = "persistence")]
         if let Some(storage) = _storage {
             *self = epi::get_value(storage, epi::APP_KEY).unwrap_or_default()
         }
@@ -44,7 +43,6 @@ impl epi::App for TemplateApp {
 
     /// Called by the frame work to save state before shutdown.
     /// Note that you must enable the `persistence` feature for this to work.
-    #[cfg(feature = "persistence")]
     fn save(&mut self, storage: &mut dyn epi::Storage) {
         epi::set_value(storage, epi::APP_KEY, self);
     }
